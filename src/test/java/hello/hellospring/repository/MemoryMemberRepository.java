967bf131608ef019df014d7da00034da20e4e4cf
choiinho <ino0319@gmail.com>
2020-10-01 23:30:17 +0900

    MemberRepository
    service 구현

--- src/test/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,11 +5,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class MemoryMemberRepository {
+class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
 
--- src/test/java/hello/hellospring/service/MemberService.java
+++ src/test/java/hello/hellospring/service/MemberService.java
@@ -2,20 +2,21 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemberRepository() {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
         /*
         * 회원가입
         * */
         public Long join(Member member) {
 
-            validateDuplicateMember(member);
+            validateDuplicateMember(member); // 중복 회원 검증
             memberRepository.save(member);
             return member.getId();
         }
@@ -23,7 +24,7 @@ public class MemberService {
         private void validateDuplicateMember(Member member) {
             memberRepository.findByName(member.getName())
                     .ifPresent(m -> {
-                        throw new IllegalStateException("이미 존재하는 회원입니다.")
+                        throw new IllegalStateException("이미 존재하는 회원입니다.");
                     });
         }
 
@@ -37,5 +38,4 @@ public class MemberService {
         public Optional<Member> findOne(Long memberId) {
             return memberRepository.findById(memberId);
         }
-
 }
