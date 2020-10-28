031b4cf23614fdfe79a84491e33348796a8f75a0
choiinho <ino0319@gmail.com>
2020-10-28 23:24:11 +0900

    Bean setting

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -3,7 +3,9 @@ package hello.hellospring;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SpringConfig {
 
     @Bean
