ede90d481aa8db7eb041b50bb8bc39ab430a5779
choiinho <ino0319@gmail.com>
2020-11-07 23:42:48 +0900

    home

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -8,8 +8,11 @@ import java.util.Optional;
 public interface MemberRepository {
 
     Member save(Member member);
+
     Optional<Member> findById(Long id); // null일 때?? optional로 감싸서...
+
     Optional<Member> findByName(String name);
-    List<Member> findAll(); 
+
+    List<Member> findAll();
 
 }
