5521e709e9543a662f7bd94a94192b9e7a23258f
choiinho <choiinho@knou.ac.kr>
2020-12-03 10:14:50 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/hello/hellospring/SpringConfig.java

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Transactional
+
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -47,11 +48,27 @@ public class MemberService {
      * */
     public List<Member> findMembers() {
 
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
 
-        return memberRepository.findById(memberId);
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findById(memberId);
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 }
