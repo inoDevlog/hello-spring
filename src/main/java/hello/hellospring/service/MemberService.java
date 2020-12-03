6d1b6db711f335923c82631ed2697e2d23638181
choiinho <choiinho@knou.ac.kr>
2020-12-03 12:03:06 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/hello/hellospring/SpringConfig.java

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -22,18 +22,10 @@ public class MemberService {
      * */
     public Long join(Member member) {
 
-        long start = System.currentTimeMillis();
-
-        try {
-            // 같은 이름이 있는 중복 회원X
-            validateDuplicateMember(member); // 중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        // 같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -47,28 +39,10 @@ public class MemberService {
      * 전체 회원 조회
      * */
     public List<Member> findMembers() {
-
-        long start = System.currentTimeMillis();
-
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
-
-        long start = System.currentTimeMillis();
-
-        try {
-            return memberRepository.findById(memberId);
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
+        return memberRepository.findById(memberId);
     }
 }
